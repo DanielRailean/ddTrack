3120aa9dc4dae00bb148498ca003940deebe0e5b
Daniel Railean <daniel.railean1@gmail.com>
2019-11-18 13:04:43 +0100

    Added routes to users and exercises and proper support in the server.js file

--- dd_track/backend/routes/users.js
+++ dd_track/backend/routes/users.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
-let User = require('..models/user.model');
+let User = require('../models/user.model');
 
-router.route('/').get((req, res) =>{
+router.route('/').get((req, res) => {
     User.find()
         .then(users => res.json(users))
         .catch(err => res.status(400).json('Error: ' + err));
         
 });
 
-router.route('/add').get((req,res) =>{
+router.route('/add').post((req,res) =>{
     const username = req.body.username;
 
     const newUser = new User({username});
