d72e35a0e21c7fcbf56cbea42cdb6a8fb1ee8a72
Daniel Railean <daniel.railean1@gmail.com>
2020-08-18 00:05:34 +0200

    Debug

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-require('dotenv').config();
+// require('dotenv').config();
+
 
 const app = express();
 const port = process.env.PORT || 5000;
