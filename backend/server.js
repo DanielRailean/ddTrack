7313dc12386b1449760c51dc9b152d3ca16d1308
Daniel Railean <daniel.railean1@gmail.com>
2020-08-17 23:59:16 +0200

    Debug

--- backend/server.js
+++ backend/server.js
@@ -10,14 +10,12 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true,});
 const connection = mongoose.connection;
 connection.once('open', () => {
    console.log("MongoDB is connected!");
 });
-mongoose.connection.on('error', (err) => { console.log('MongoDB connection error: ', err); }); 
 
 const exercisesRouter = require('./routes/exercises');
 const usersRouter = require('./routes/users');
@@ -25,6 +23,6 @@ const usersRouter = require('./routes/users');
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(port, () => {
    console.log(`Server is running on port: ${port}`)
 }); 
