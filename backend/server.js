30ab85aeb297689179cc0174c4e1f37e065627e1
Daniel Railean <daniel.railean1@gmail.com>
2020-08-18 00:12:48 +0200

    Debug

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
